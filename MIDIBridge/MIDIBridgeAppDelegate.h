a73ebed4cac14501aa3f1caa9a0de1c8e1e34c3c
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-11-01 17:26:37 +0900

    Temporary implemented MIDI input interface.

--- MIDIBridge/MIDIBridgeAppDelegate.h
+++ MIDIBridge/MIDIBridgeAppDelegate.h
@@ -1,15 +1,19 @@
-//
-//  MIDIBridgeAppDelegate.h
-//  MIDIBridge
-//
-//  Created by Keijiro Takahashi on 11/1/13.
-//  Copyright (c) 2013 Keijiro Takahashi. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
+#import <CoreMIDI/CoreMIDI.h>
 
 @interface MIDIBridgeAppDelegate : NSObject <NSApplicationDelegate>
+{
+    int _inSocket;
+    int _outSocket;
+    dispatch_source_t _inSource;
+    dispatch_source_t _outSource;
+    MIDIClientRef _midiClient;
+    MIDIPortRef _midiInputPort;
+    MIDIUniqueID _sourceIDs[256];
+}
 
 @property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSTableView *deviceTable;
+@property (assign) IBOutlet NSTextView *textView;
 
 @end
--- MIDIBridge/MIDIBridgeAppDelegate.m
+++ MIDIBridge/MIDIBridgeAppDelegate.m
@@ -1,18 +1,230 @@
-//
-//  MIDIBridgeAppDelegate.m
-//  MIDIBridge
-//
-//  Created by Keijiro Takahashi on 11/1/13.
-//  Copyright (c) 2013 Keijiro Takahashi. All rights reserved.
-//
-
 #import "MIDIBridgeAppDelegate.h"
+#import <CoreMIDI/CoreMIDI.h>
+#import <arpa/inet.h>
+
+#pragma mark Configuration
+
+#define MIDI_IN_PORT 52364
+#define MIDI_OUT_PORT 52365
+
+#pragma mark Local data structure
+
+struct MIDIMessage
+{
+    MIDIUniqueID source;
+    Byte status;
+    Byte data[2];
+    Byte padding;
+};
+
+#pragma mark Private method definition
+
+@interface MIDIBridgeAppDelegate ()
+
+- (void)resetStatus;
+- (void)processIncoming:(NSData *)data;
+
+@end
+
+#pragma mark
+#pragma mark Core MIDI callbacks
+
+static void MyMIDIStateChangedHander(const MIDINotification* message, void* refCon)
+{
+    // Only process additions and removals.
+    if (message->messageID != kMIDIMsgObjectAdded && message->messageID != kMIDIMsgObjectRemoved) return;
+    
+    // Only process source operations.
+    const MIDIObjectAddRemoveNotification *addRemoveDetail = (const MIDIObjectAddRemoveNotification *)(message);
+    if (addRemoveDetail->childType != kMIDIObjectType_Source) return;
+    
+    // Reset the client status (on the main thread).
+    dispatch_async(dispatch_get_main_queue(), ^{
+        MIDIBridgeAppDelegate *delegate = (__bridge MIDIBridgeAppDelegate *)(refCon);
+        [delegate resetStatus];
+    });
+}
+
+static void MyMIDIReadProc(const MIDIPacketList *packetList, void *readProcRefCon, void *srcConnRefCon)
+{
+    MIDIUniqueID sourceID = *(MIDIUniqueID *)srcConnRefCon;
+    
+    NSMutableData *data = [NSMutableData data];
+    
+    // Transform the packets into MIDI messages and push it to the message queue.
+    const MIDIPacket *packet = &packetList->packet[0];
+    for (int packetCount = 0; packetCount < packetList->numPackets; packetCount++) {
+        // Extract MIDI messages from the data stream.
+        for (int offs = 0; offs < packet->length;) {
+            struct MIDIMessage msg;
+            msg.source = sourceID;
+            msg.status = packet->data[offs++];
+            msg.data[0] = 0;
+            msg.data[1] = 0;
+            
+            for (int dc = 0; offs < packet->length && (packet->data[offs] < 0x80); dc++, offs++) {
+                if (dc < 2) msg.data[dc] = packet->data[offs];
+            }
+            
+            [data appendBytes:&msg length:sizeof(msg)];
+        }
+        packet = MIDIPacketNext(packet);
+    }
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        MIDIBridgeAppDelegate *delegate = (__bridge MIDIBridgeAppDelegate *)(readProcRefCon);
+        [delegate processIncoming:data];
+    });
+}
+
+#pragma mark
 
 @implementation MIDIBridgeAppDelegate
 
+#pragma mark Application Delegate
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    [self resetStatus];
+}
+
+- (void)resetStatus
+{
+    // Dispose the client if already initialized.
+    if (_midiClient) MIDIClientDispose(_midiClient);
+    
+    // Create a MIDI client.
+    MIDIClientCreate(CFSTR("MIDIBridge Client"), MyMIDIStateChangedHander, (__bridge void *)(self), &_midiClient);
+    
+    // Create a MIDI port which covers all MIDI sources.
+    MIDIInputPortCreate(_midiClient, CFSTR("MIDIBridge Input Port"), MyMIDIReadProc, (__bridge void *)(self), &_midiInputPort);
+    
+    // Enumerate the all MIDI sources.
+    ItemCount sourceCount = MIDIGetNumberOfSources();
+    NSAssert(sourceCount < sizeof(_sourceIDs) / sizeof(MIDIUniqueID), @"Too many MIDI sources.");
+    
+    for (int i = 0; i < sourceCount; i++) {
+        // Connect the MIDI source to the input port.
+        MIDIEndpointRef source = MIDIGetSource(i);
+        MIDIObjectGetIntegerProperty(source, kMIDIPropertyUniqueID, &_sourceIDs[i]);
+        MIDIPortConnectSource(_midiInputPort, source, &_sourceIDs[i]);
+    }
+
+    [self.deviceTable reloadData];
+}
+
+- (void)processIncoming:(NSData *)data
+{
+    NSLog(@"data:%lu", (unsigned long)[data length]);
+}
+
+#pragma mark Table View Data Soruce methods
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    return _midiClient ? MIDIGetNumberOfSources() : 0;
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    if (!_midiClient) return nil;
+    
+    int columnID = [[tableColumn identifier] intValue];
+    
+    if (columnID == 2) {
+        // 2nd column: shows the display name.
+        MIDIObjectRef object;
+        MIDIObjectType type;
+        MIDIObjectFindByUniqueID(_sourceIDs[row], &object, &type);
+        NSAssert(type == kMIDIObjectType_Source, @"Invalid ID.");
+        
+        CFStringRef name;
+        MIDIObjectGetStringProperty(object, kMIDIPropertyDisplayName, &name);
+        
+        return (NSString*)CFBridgingRelease(name);
+    }
+    
+    // (1st column) show the endpoint ID.
+    return [NSString stringWithFormat:@"%0x", _sourceIDs[row]];
 }
 
 @end
+
+#pragma mark
+#pragma mark Memo :)
+
+#if 0
+// Create the MIDI-in socket.
+{
+    _inSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    NSAssert(_inSocket >= 0, @"Failed to create a socket (%d).", errno);
+    
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = 0;
+    
+    int err = bind(_inSocket, (struct sockaddr *)&addr, sizeof(addr));
+    NSAssert(err == 0, @"Failed to bind a socket (%d).", errno);
+}
+
+// Create the MIDI-out socket.
+{
+    _outSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    NSAssert(_outSocket >= 0, @"Failed to create a socket (%d).", errno);
+    
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(MIDI_OUT_PORT);
+    
+    int err = bind(_outSocket, (struct sockaddr *)&addr, sizeof(addr));
+    NSAssert(err == 0, @"Failed to bind a socket (%d).", errno);
+}
+
+// Uses the default queue to dispatching.
+dispatch_queue_t defaultQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+
+// Start the MIDI-in handler.
+_inSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, defaultQueue);
+dispatch_source_set_timer(_inSource, DISPATCH_TIME_NOW, NSEC_PER_SEC / 3, NSEC_PER_SEC / 2);
+{
+    // sockaddr for MIDI-in.
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    inet_aton("127.0.0.1", &(addr.sin_addr));
+    addr.sin_port = htons(MIDI_IN_PORT);
+    
+    // Other local variables for the handler.
+    __block int counter = 0;
+    
+    // The handler block.
+    dispatch_source_set_event_handler(_inSource, ^{
+        char buffer[256];
+        snprintf(buffer, sizeof(buffer), "Hooray! %d", counter);
+        sendto(_inSocket, buffer, strlen(buffer), MSG_DONTWAIT, (struct sockaddr *)&addr, sizeof(addr));
+        counter++;
+    });
+}
+dispatch_resume(_inSource);
+
+// Start the MIDI-out handler.
+_outSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_READ, _outSocket, 0, defaultQueue);
+dispatch_source_set_event_handler(_outSource, ^{
+    char buffer[1024];
+    size_t estimated = dispatch_source_get_data(_outSource);
+    recv(_outSocket, buffer, estimated, 0);
+    buffer[estimated] = 0;
+    
+    NSAttributedString *string = [[NSAttributedString alloc] initWithString:[[NSString stringWithUTF8String:buffer] stringByAppendingString:@"\n"]];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.textView.textStorage beginEditing];
+        [self.textView.textStorage appendAttributedString:string];
+        [self.textView.textStorage endEditing];
+        [self.textView scrollRangeToVisible:NSMakeRange(self.textView.string.length, 0)];
+    });
+});
+dispatch_resume(_outSource);
+#endif
