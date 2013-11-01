66685add12ec0b787d6b591249943b1146df13cd
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-11-01 15:53:55 +0900

    Initial Commit

---
+++ MIDIBridge/MIDIBridgeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MIDIBridgeAppDelegate.h
+//  MIDIBridge
+//
+//  Created by Keijiro Takahashi on 11/1/13.
+//  Copyright (c) 2013 Keijiro Takahashi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MIDIBridgeAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MIDIBridge/MIDIBridgeAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MIDIBridgeAppDelegate.m
+//  MIDIBridge
+//
+//  Created by Keijiro Takahashi on 11/1/13.
+//  Copyright (c) 2013 Keijiro Takahashi. All rights reserved.
+//
+
+#import "MIDIBridgeAppDelegate.h"
+
+@implementation MIDIBridgeAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
